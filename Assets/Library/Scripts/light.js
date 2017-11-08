f99959e9e8257022d3d7e4a747ab3eb441140367
Jonatan Lemos Zuluaga <jonatan.lemos.z@gmail.com>
2017-11-07 23:56:44 -0700

    Added Library asssets

---
+++ Assets/Library/Scripts/light.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+private var glow : Light;
+var Intensity : float = 2.0;
+var IntensityDefault : float = 4.0;
+var time : float = 3;
+private var down : boolean = false;
+function Start () {
+	glow = GetComponent.<Light>();
+
+
+}
+
+function Update () {
+	if(glow.intensity <= Intensity  && down == false){
+		glow.intensity += time * Time.deltaTime;
+	}
+	if(glow.intensity > Intensity && down == false){
+		down = true;
+	}
+
+	if(down){
+		glow.intensity -= time * Time.deltaTime;
+		if(glow.intensity <= IntensityDefault){
+			down = false;
+		}
+	}
+}
