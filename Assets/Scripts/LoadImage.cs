634803034600a9bc3f0f0422c6f4c81c82e170f3
Jonatan Lemos Zuluaga <jlemoszu@Jonatans-MacBook-Pro.local>
2017-11-14 23:31:22 -0700

    Added support for loading images from the web

---
+++ Assets/Scripts/LoadImage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadImage : MonoBehaviour {
+
+	public string url = "";
+	IEnumerator Start()
+	{
+		WWW www = new WWW(url);
+		yield return www;
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = www.texture;
+	}
+}
